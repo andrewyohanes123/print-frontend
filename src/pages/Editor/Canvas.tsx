15a81a0196b1e99b8f8e69f40999f50034d85560
andrewyohanes123 <andrew.yohanes123@gmail.com>
2021-07-26 12:30:06 +0800

    +working dynamic canvas image +cloth list for public user +step +empty cloth side list component

--- src/pages/Editor/Canvas.tsx
+++ src/pages/Editor/Canvas.tsx
@@ -1,30 +1,34 @@
-import { FC, ReactElement, useRef, useEffect, useState, useCallback } from "react"
+import { FC, ReactElement, useRef, useEffect, useState, useCallback, useLayoutEffect, useContext } from "react"
 import { Stage, Layer, Image as Img, Rect, Transformer as Tr } from 'react-konva'
 import { Button, Divider, FlexboxGrid } from 'rsuite'
 import useImage from "use-image";
 import { Transformer } from "konva/lib/shapes/Transformer";
 import { Image } from "konva/lib/shapes/Image";
 import { CirclePicker } from 'react-color'
-import front from 'assets/front.png';
-import bg from 'assets/background-front.png';
 import logo from 'assets/coffee.png'
 import { KonvaEventObject } from "konva/lib/Node";
 import useModels from "hooks/useModels";
 import useErrorCatcher from "hooks/useErrorCatcher";
+import { EditorContext } from ".";
+import { baseUrl } from "App";
+import CanvasImage from "./CanvasImage";
 
 const Canvas: FC = (): ReactElement => {
-  const [cloth] = useImage(front);
-  const [background] = useImage(bg);
   const [coffee] = useImage(logo);
   const trRef = useRef<Transformer>(null);
   const imgRef = useRef<Image>(null);
+  const [clothBase, setClothBase] = useState<string>('');
+  const [clothBackground, setClothBackground] = useState<string>('');
+  const flexBoxRef = useRef<HTMLDivElement>(null);
+  const [canvasSize, setCanvasSize] = useState<number>(500);
   const [clothColor, setClothColor] = useState<string>('white');
   const [preview, togglePreview] = useState<boolean>(false);
   const [coords, setCoords] = useState<{ x: number, y: number }>({ x: 0, y: 0 });
   const [scale, setScale] = useState<{ width: number; height: number }>({ width: 400, height: 400 });
   const [colors, setColors] = useState<string[]>(['white']);
-  const { models: { Color } } = useModels();
-  const {errorCatch} = useErrorCatcher();
+  const { models: { Color, ClothSide } } = useModels();
+  const { errorCatch } = useErrorCatcher();
+  const { cloth_id } = useContext(EditorContext);
 
   const getColors = useCallback(() => {
     Color.collection({
@@ -36,11 +40,40 @@ const Canvas: FC = (): ReactElement => {
     })
   }, [errorCatch, Color]);
 
+  const getFirstClothSide = useCallback(() => {
+    if (typeof cloth_id !== 'undefined') {
+      ClothSide.collection({
+        limit: 1,
+        attributes: ['cloth_base', 'cloth_background', 'cloth_id'],
+        where: {
+          cloth_id
+        }
+      }).then(resp => {
+        const [side] = resp.rows;
+        setClothBase(`${baseUrl}/public/files/${side.cloth_base}`);
+        setClothBackground(`${baseUrl}/public/files/${side.cloth_background}`);
+      }).catch(e => {
+        errorCatch(e);
+      })
+    }
+  }, [cloth_id, errorCatch]);
+
+  useEffect(() => {
+    getFirstClothSide();
+  }, [getFirstClothSide])
+
   useEffect(() => {
     getColors();
   }, [getColors]);
 
-  useEffect(() => {
+  useLayoutEffect(() => {
+    if (flexBoxRef.current !== null) {
+      setCanvasSize(flexBoxRef.current.clientWidth);
+      // console.log(flexBoxRef.current.clientWidth)
+    }
+  }, [flexBoxRef])
+
+  useLayoutEffect(() => {
     if (trRef.current !== null && imgRef.current !== null && !preview) {
       trRef.current.nodes([imgRef.current]);
       trRef.current.getLayer()?.batchDraw();
@@ -67,29 +100,41 @@ const Canvas: FC = (): ReactElement => {
     }
   }, [imgRef]);
 
+  // const canvasScale: number = useMemo(() => {
+  //   return Math.min(
+  //     canvasSize / 700,
+  //     canvasSize / 700
+  //   )
+  // },[canvasSize])
+
   return (
     <FlexboxGrid.Item colspan={11}>
-      <Stage width={640} height={800}>
-        <Layer>
-          <Rect width={640} height={800} fill={clothColor} />
-          {preview &&
+      <div ref={flexBoxRef}>
+        <Stage width={canvasSize} height={canvasSize}>
+          <Layer>
+            <Rect width={640} height={800} fill={clothColor} />
+            {preview &&
+              <>
+                <Img draggable {...coords} image={coffee} {...scale} />
+              </>}
+            <CanvasImage src={clothBase} key={clothBase} globalCompositeOperation="overlay" opacity={0.45} canvasSize={canvasSize} />
+            <CanvasImage src={clothBase} globalCompositeOperation="multiply" opacity={0.8} canvasSize={canvasSize} />
+            <CanvasImage src={clothBackground} key={clothBackground} canvasSize={canvasSize} />
+            {/* <Img width={canvasSize} height={canvasSize} globalCompositeOperation="overlay" opacity={0.3} image={cloth} /> */}
+            {/* <Img width={canvasSize} height={canvasSize} globalCompositeOperation="multiply" opacity={0.75} image={cloth} /> */}
+            {/* <Img width={canvasSize} height={canvasSize} image={background} /> */}
+          </Layer>
+          {!preview && <Layer>
             <>
-              <Img draggable {...coords} image={coffee} {...scale} />
-            </>}
-          <Img width={640} height={800} globalCompositeOperation="overlay" opacity={0.3} image={cloth} />
-          <Img width={640} height={800} globalCompositeOperation="multiply" opacity={0.75} image={cloth} />
-          <Img width={640} height={800} image={background} />
-        </Layer>
-        {!preview && <Layer>
-          <>
-            <Img draggable onDragEnd={onDragEnd} onTransformEnd={onTransformEnd} ref={imgRef} image={coffee} {...coords} {...scale} />
-            <Tr ref={trRef} />
-          </>
-        </Layer>}
-      </Stage>
-      <Divider />
-      <Button style={{ marginBottom: 8 }} onClick={() => togglePreview(!preview)} appearance="primary" block>{preview ? "Edit Mockup" : `Preview Mockup`}</Button>
-      <CirclePicker circleSpacing={20} circleSize={35} width="100%" colors={colors} color={clothColor} onChangeComplete={ev => setClothColor(ev.hex)} />
+              <Img draggable onDragEnd={onDragEnd} onTransformEnd={onTransformEnd} ref={imgRef} image={coffee} {...coords} {...scale} />
+              <Tr ref={trRef} />
+            </>
+          </Layer>}
+        </Stage>
+        <Divider />
+        <Button style={{ marginBottom: 8 }} onClick={() => togglePreview(!preview)} appearance="primary" block>{preview ? "Edit Mockup" : `Preview Mockup`}</Button>
+        <CirclePicker circleSpacing={20} circleSize={35} width="100%" colors={colors} color={clothColor} onChangeComplete={ev => setClothColor(ev.hex)} />
+      </div>
     </FlexboxGrid.Item>
   )
 }
